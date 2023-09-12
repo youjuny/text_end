366fd8b060063a41819aaab418b465a059d8cc6f
youjuny <youyoung2774@gmail.com>
2023-09-12 15:42:07 +0900

    게시물만들기2

---
+++ src/main/java/Article.java
@@ -0,0 +1,39 @@
+public class Article {
+
+    // 게시물 번호
+    private int id;
+    // 제목
+    private String title;
+    // 내용
+    private String content;
+
+    public Article(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,84 +2,129 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    static ArrayList<Article> articles = new ArrayList<>();
+
     public static void main(String[] args) {
-        ArrayList<String> titles = new ArrayList<>();
-        ArrayList<String> contents = new ArrayList<>();
-        int articleNum = 1;
+
+        Scanner scan = new Scanner(System.in);
+        int lastArticleId = 1;
 
         while (true) {
-            System.out.println("메뉴 입력:");
-            Scanner scan = new Scanner(System.in);
+            System.out.print("명령어 : ");
             String command = scan.nextLine();
-
-            if (command.equals("a")) {
-                System.out.println("기능1");
-            } else if (command.equals("b")) {
-                System.out.println("기능2");
+            if (command.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
             } else if (command.equals("help")) {
-                System.out.println("a : 기능1");
-                System.out.println("b : 기능2");
-                System.out.println("exit : 종료");
-                System.out.println("add : 게시물 추가");
-                System.out.println("list : 게시물 조회하기");
-                System.out.println("update : 게시물 수정하기");
-                System.out.println("delete : 게시물 삭제하기");
-            } else if (command.equals("add")) {
-                System.out.printf("게시물 제목을 입력해주세요: ");
+                System.out.println("add : 게시물을 추가합니다.");
+                System.out.println("update : 게시물을 수정합니다.");
+                System.out.println("test : 테스트 게시물을 추가합니다.");
+                System.out.println("delete : 게시물을 삭제합니다.");
+                System.out.println("detail : 게시물 세부정보를 확인합니다.");
+
+            }
+            else if (command.equals("add")) {
+                System.out.print("게시물 제목을 입력해주세요 : ");
                 String title = scan.nextLine();
-                System.out.printf("게시물 내용을 입력해주세요: ");
+                System.out.print("게시물 내용을 입력해주세요 : ");
                 String content = scan.nextLine();
 
-                titles.add(title);
-                contents.add(content);
-                articleNum++;
+                Article article = new Article(lastArticleId, title, content);
+                articles.add(article);
 
+                System.out.println("게시물이 등록되었습니다.");
+                lastArticleId++;
             } else if (command.equals("list")) {
-                System.out.println("=============");
-                for (int i = 0; i < titles.size(); i++) {
-                    System.out.printf("번호 : %d\n", i + 1); //
-                    System.out.printf("제목 : %s\n", titles.get(i));
-                    System.out.printf("내용 : %s\n", contents.get(i));
-                    System.out.println("=============");
+                System.out.println("==================");
+                for (int i = 0; i < articles.size(); i++) {
+
+                    Article article = articles.get(i);
+
+                    System.out.printf("번호 : %d\n", article.getId());
+                    System.out.printf("제목 : %s\n", article.getTitle());
+                    System.out.println("==================");
                 }
             } else if (command.equals("update")) {
-                System.out.printf("수정할 게시물 번호 : ");
-                int updateArticleNumber = scan.nextInt();
-
-                scan.nextLine(); //// 제목이랑 내용 같은 줄에 안나오게 하기
-
-                if (updateArticleNumber >= 1 && updateArticleNumber <= titles.size()) {
-                    System.out.printf("새로운 게시물 제목을 입력해주세요 : ");
-                    String newTitle = scan.nextLine();
-                    System.out.printf("새로운 게시물 내용을 입력해주세요 : ");
-                    String newContent = scan.nextLine();
-
-                    titles.set(updateArticleNumber - 1, newTitle);
-                    contents.set(updateArticleNumber - 1, newContent);
-                    System.out.printf("%d번 게시물이 수정되었습니다.\n", updateArticleNumber);
-
-                } else {
-                    System.out.println("없는 게시물번호입니다.");
-                }
-            } else if (command.equals("delete")) {
-                System.out.println("삭제할 게시물 번호 : ");
-                int deleteArticleNumber = scan.nextInt();
+                System.out.print("수정할 게시물 번호 : ");
+                int targetId = scan.nextInt();
 
                 scan.nextLine();
 
-                if (deleteArticleNumber >= 1 && deleteArticleNumber<=titles.size()) {
-                    titles.remove(deleteArticleNumber - 1);
-                    contents.remove(deleteArticleNumber -1);
-                    System.out.printf("%d번 게시물이 삭제되었습니다.\n",deleteArticleNumber);
-                }else {
-                    System.out.println("없는 게시물 번호입니다.");
+                boolean isExist = false;
+
+                for (int i = 0; i < articles.size(); i++) {
+                    Article article = articles.get(i);
+
+                    if (targetId == article.getId()) {
+                        System.out.print("제목 : ");
+                        String newTitle = scan.nextLine();
+                        System.out.print("내용 : ");
+                        String newContent = scan.nextLine();
+
+                        Article newArticle = new Article(targetId, newTitle, newContent);
+                        articles.set(i, newArticle);
+
+                        System.out.println("수정이 완료되었습니다.");
+                        isExist = true;
+                    }
                 }
 
+                if (isExist == false) {
+                    System.out.println("없는 게시물입니다.");
+                }
+            } else if (command.equals("delete")) {
+                System.out.print("삭제할 게시물 번호 : ");
+                int targetId = scan.nextInt();
 
-            } else if (command.equals("exit")) {
-                System.out.println("프로그램을 종료합니다.");
-                break;
+                scan.nextLine();
+
+                boolean isExist = false;
+
+                for (int i = 0; i < articles.size(); i++) {
+                    Article article = articles.get(i);
+
+                    if (targetId == article.getId()) {
+                        articles.remove(i);
+                        System.out.printf("%d번 게시물이 삭제되었습니다.\n", targetId);
+                        isExist = true;
+                    }
+                }
+
+                if (isExist == false) {
+                    System.out.println("없는 게시물입니다.");
+                }
+            } else if (command.equals("detail")) {
+                // 중복 -> 함수
+
+                System.out.print("상세보기 할 게시물 번호를 입력해주세요 : ");
+                int targetId = scan.nextInt();
+                Article article = findById(targetId);
+
+                if (article == null) {
+                    System.out.println("존재하지 않는 게시물입니다.");
+                } else {
+                    System.out.println("===================");
+                    System.out.printf("번호 : %d\n", article.getId());
+                    System.out.printf("제목 : %s\n", article.getTitle());
+                    System.out.printf("내용 : %s\n", article.getContent());
+                    System.out.println("===================");
+                }
             }
         }
+    }
+
+    public static Article findById(int id) {
+
+        Article target = null;
+
+        for(int i = 0; i < articles.size(); i++) {
+            Article article = articles.get(i);
+            if(id == article.getId()) {
+                target = article;
+            }
+        }
+
+        return target;
+
     }
 }
